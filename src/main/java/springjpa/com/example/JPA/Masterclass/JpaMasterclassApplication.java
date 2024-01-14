84e6928ed8f776851ce5b4f156caa5b5d0936b24
imskanand <the.anand3099@gmail.com>
2024-01-15 00:05:47 +0530

    pagination

--- src/main/java/springjpa/com/example/JPA/Masterclass/JpaMasterclassApplication.java
+++ src/main/java/springjpa/com/example/JPA/Masterclass/JpaMasterclassApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
@@ -18,12 +20,23 @@ public class JpaMasterclassApplication {
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
 		return args -> {
 			generateRandomStudents(studentRepository);
-			Sort sort = Sort.by("firstName").ascending()
-					.and(Sort.by("age").descending());
-			studentRepository
-					.findAll(sort)
-					.forEach(student -> System.out.println(student.getFirstName()+ " " + student.getAge()));
+//			getStudentDataSortedByFirstNameAndAge(studentRepository);
+
+			PageRequest pageRequest = PageRequest.of(
+					0,
+					5,
+					Sort.by("firstName").ascending());
+			Page<Student> page = studentRepository.findAll(pageRequest);
+			System.out.println(page);
 		};
+	}
+
+	private static void getStudentDataSortedByFirstNameAndAge(StudentRepository studentRepository) {
+		Sort sort = Sort.by("firstName").ascending()
+				.and(Sort.by("age").descending());
+		studentRepository
+				.findAll(sort)
+				.forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
 	}
 
 	private static void generateRandomStudents(StudentRepository studentRepository) {
