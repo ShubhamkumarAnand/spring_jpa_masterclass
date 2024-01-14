3508eb32e0bc709aa821df88acfb867b0566dfde
imskanand <the.anand3099@gmail.com>
2024-01-14 21:12:33 +0530

    Exploring the jpa repository methods

--- src/main/java/springjpa/com/example/JPA/Masterclass/JpaMasterclassApplication.java
+++ src/main/java/springjpa/com/example/JPA/Masterclass/JpaMasterclassApplication.java
@@ -1,12 +1,44 @@
 package springjpa.com.example.JPA.Masterclass;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class JpaMasterclassApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaMasterclassApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+		return args -> {
+			Student maria = new Student("maria", "jon", "maria@spring.edu", 24);
+			Student ram = new Student("ram", "chandra", "ram@ramayan.com", 21);
+			studentRepository.saveAll(List.of(maria, ram));
+			System.out.println(studentRepository.count());
+
+			studentRepository
+					.findById(2L)
+					.ifPresentOrElse(
+							System.out::println,
+							() -> System.out.println("Student with id = 2 not Found"));
+
+			studentRepository
+					.findById(3L)
+					.ifPresentOrElse(
+							System.out::println,
+							() -> System.out.println("Student with id = 3 not Found"));
+
+			List<Student> students = studentRepository.findAll();
+			students.forEach(System.out::println);
+
+			studentRepository.deleteById(1L);
+			System.out.println(studentRepository.count());
+		};
 	}
 }
---
+++ src/main/java/springjpa/com/example/JPA/Masterclass/StudentRepository.java
@@ -0,0 +1,6 @@
+package springjpa.com.example.JPA.Masterclass;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
