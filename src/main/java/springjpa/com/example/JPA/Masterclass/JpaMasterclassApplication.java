5057bc986a49a3b9b94fe0e16624357d2b28a4ee
imskanand <the.anand3099@gmail.com>
2024-01-14 19:11:07 +0530

    Initial Commit

---
+++ src/main/java/springjpa/com/example/JPA/Masterclass/JpaMasterclassApplication.java
@@ -0,0 +1,13 @@
+package springjpa.com.example.JPA.Masterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMasterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMasterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springjpa/com/example/JPA/Masterclass/JpaMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package springjpa.com.example.JPA.Masterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
